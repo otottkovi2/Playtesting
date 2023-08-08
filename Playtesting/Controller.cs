9393acd298085cfe67ace540a2aa94350ba0cb2f
otottkovi2 <otottkovi@hotmail.com>
2023-08-08 17:29:33 +0200

    Initial commit

---
+++ Playtesting/Controller.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Playtesting
+{
+    public class Controller
+    {
+        private List<Tester> testers = new();
+
+    }
+}
---
+++ Playtesting/Tester.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace Playtesting
+{
+    public record Tester(string Name, byte Age, string Version, float PlayTime, byte HwTier, byte PerformanceScore,
+        byte GameplayScore, byte StoryScore, byte VisualsScore, byte MusicScore);
+}
